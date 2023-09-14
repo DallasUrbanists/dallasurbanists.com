be0de2145e40fc5b0918cfa3fbc4d4042c172872
Angela Tran <angelattran0+git@gmail.com>
2023-09-14 09:20:32 +0000

    Wrote a custom Jekyll plugin to generate /tagged pages

---
+++ _plugins/tag_page_generator.rb
@@ -0,0 +1,36 @@
+module TagPageGeneratorPlugin
+    class TagPageGenerator < Jekyll::Generator
+      safe true
+  
+      def generate(site)
+        site.tags.each do |tag, posts|
+          site.pages << TagPage.new(site, tag, posts)
+        end
+      end
+    end
+  
+    # Subclass of `Jekyll::Page` with custom method definitions.
+    class TagPage < Jekyll::Page
+      def initialize(site, tag, posts)
+        @site = site             # the current site instance.
+        @base = site.source      # path to the source directory.
+        @dir  = "tagged"         # the directory the page will reside in.
+  
+        @basename = tag      # filename without the extension.
+        @ext      = '.html'      # the extension.
+        @name     = '#(tag).html' # basically @basename + @ext.
+
+        item = (site.data["new_tags"]["items"].find_all {|t| t["name"] == tag})&.last()
+        
+        @data = {
+            "layout" => "tag-feed",
+            "tag_feed" => tag,
+            "title" => tag
+        }
+        
+        if item != nil
+            data.store("description", item["description"])
+        end
+      end
+    end
+  end

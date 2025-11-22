63930b31361a56ad4b8e018681de073f5cedbe10
Hexel Colorado <hexelcolorado@gmail.com>
2025-11-22 01:31:12 +0000

    jekyll logger fix

---
+++ jekyll_logger_fix.rb
@@ -0,0 +1,19 @@
+begin
+  require 'logger'
+
+  class ::Logger
+    # Make `level` robust if an initializer didn't set `@level_override`.
+    # This happens when subclasses (like Jekyll::Stevenson) override
+    # `initialize` without calling `super` (Ruby 3.3 introduced
+    # `@level_override` usage in Logger#level).
+    unless instance_methods(false).include?(:__patched_level)
+      alias_method :__patched_level, :level
+
+      def level
+        (@level_override ||= {})[Fiber.current] || @level
+      end
+    end
+  end
+rescue LoadError
+  # If logger isn't available for some reason, skip patch.
+end
